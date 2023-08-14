fda49557f2677b8cbda0f77fe7cbce6d4799dfbc
kaverinstudio <kaverinstudio.gmail.com>
2023-08-14 22:15:38 +0600

    User check login

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import './assets/scss/app.scss';
 import Sidebar from "./components/Sidebar";
 import Main from "./components/pages/Main";
 import User from "./components/pages/user/User";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {auth} from "./assets/api/user";
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(auth())
+    },[])
+
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
 
     if (!isAuthenticated) {
         return <User />;
     }
+
 
     return (
         <Box className='wrapper'>
